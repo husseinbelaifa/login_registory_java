7d81ffdd1034a67fc70fdafccdde25604959309b
belaifa_el_hussein <belaifahocine@gmail.com>
2019-02-02 09:48:40 +0100

    connect and create user table

--- src/login_regiter/connect.java
+++ src/login_regiter/connect.java
@@ -1,5 +1,52 @@
 package login_regiter;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class connect {
+	 Connection conn;
+	 Statement stmt;
+	 
+	 public connect() {
+		 conn = null;
+		 stmt=null;
+	 }
+	
+	public void  connexion() throws ClassNotFoundException {
+		
+		try {
+			Class.forName("org.sqlite.JDBC");
+			   String url = "jdbc:sqlite:login_register.db";
+	            // create a connection to the database
+	            conn = DriverManager.getConnection(url);
+	            
+	            System.out.println("Connection to SQLite has been established.");
+		}catch(SQLException e) {
+			System.out.print("Connexion error"+e);
+		}
+	}
+	
+	public void create_table() throws ClassNotFoundException {
+		
+		try {
+			
+		this.connexion();
+		 stmt = this.conn.createStatement();
+		  String sql = "CREATE TABLE user " +
+                  "(id INT NOT NULL primary key," +
+                  " username        varchar(50) NOT NULL, " + 
+                  " password varchar(50)     NOT NULL)" ;
+                
+		  stmt.executeUpdate(sql);
+	     stmt.close();
+	     System.out.print("table crée");
+		}catch(SQLException e) {
+			System.out.print("Error in creation of user table"+e);
+		}
+	         
+		
+	}
 
 }
--- src/login_regiter/main_login_register.java
+++ src/login_regiter/main_login_register.java
@@ -2,9 +2,11 @@ package login_regiter;
 
 public class main_login_register {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		// TODO Auto-generated method stub
-
+		connect con=new connect();
+	
+		con.create_table();
 	}
 
 }
